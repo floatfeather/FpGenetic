a0d9d9eb40421785a6cbce7357fd6750a1f1f5eb
floatfeather <lilianwangran@163.com>
2017-05-30 01:01:10 -0700

    Record execution time

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 using namespace std;
 using namespace genetic;
@@ -14,12 +15,15 @@ int main(int argc, char const *argv[])
 		cerr << "Invalid number of arguments." << endl;
 		return 0;
 	}
+	time_t start, end;
+	time(&start);
 	RunnerOptions options;
 	options.SetFilename(string(argv[1]));
 	GeneticRunner g_runner(options, string(argv[2]));
 	Status status = g_runner.Start();
 	if (status.OK()) {
 		Gene g = g_runner.GetBestGene();
+		time(&end);
 		char filename[100] = "geneticlog/";
 		strcat(filename, argv[2]);
 		strcat(filename , "/");
@@ -30,16 +34,19 @@ int main(int argc, char const *argv[])
 			out << arg->DebugString() << " ";
 		}
 		out << endl;
+		out << difftime(end, start) << endl;
 		out << g.frac_ << "e" << g.exp_ << endl;
 		out.close();
 	} else {
 		cerr << status.ErrorMessage() << endl;
 	}
 
+	time(&start);
 	StdGeneticRunner std_g_runner(options, string(argv[2]));
 	status = std_g_runner.Start();
 	if (status.OK()) {
 		Gene g = std_g_runner.GetBestGene();
+		time(&end);
 		char filename[100] = "stdgeneticlog/";
 		strcat(filename, argv[2]);
 		strcat(filename , "/");
@@ -50,15 +57,18 @@ int main(int argc, char const *argv[])
 			out << arg->DebugString() << " ";
 		}
 		out << endl;
+		out << difftime(end, start) << endl;
 		out << g.frac_ << "e" << g.exp_ << endl;
 		out.close();
 	}
 
+	time(&start);
 	options.SetMaxInitPopulations(options.MaxPopulations());
 	RandomRunner r_runner(options, string(argv[2]));
 	status = r_runner.Start();
 	if (status.OK()) {
 		Gene g = r_runner.GetBestGene();
+		time(&end);
 		char filename[100] = "randomlog/";
 		strcat(filename, argv[2]);
 		strcat(filename , "/");
@@ -69,6 +79,7 @@ int main(int argc, char const *argv[])
 			out << arg->DebugString() << " ";
 		}
 		out << endl;
+		out << difftime(end, start) << endl;
 		out << g.frac_ << "e" << g.exp_ << endl;
 		out.close();
 	}
